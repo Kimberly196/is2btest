0db1a9bbc6ee77937da9979f610169073969a7f0
Kimberly Delgado <Kimberly Delgado@10.9.0.94>
2016-01-20 18:59:16 -0500

    commint inicial del proyecto

---
+++ IS2TEST/src/main/java/com/mycompany/is2test/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.is2test;
+
+/**
+ *
+ * @author Kimberly Delgado
+ */
+public class Main {
+    
+    public void Saludo(){
+        
+    }
+    
+    public void Despedida(){
+        
+    } 
+    
+}
